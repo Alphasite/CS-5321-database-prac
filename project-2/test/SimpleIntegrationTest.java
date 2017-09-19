412ecd53cb46c906fb244b22757edd8daa0d7b49
Nishad <nishadmathur@gmail.com>
2017-09-19 15:04:08 -0400

    added test.

--- project-2/test/SimpleIntegrationTest.java
+++ project-2/test/SimpleIntegrationTest.java
@@ -1,42 +1,89 @@
 import datastore.Database;
 import datastore.Table;
+import datastore.TableHeader;
 import datastore.Tuple;
 import operators.bag.Join;
+import operators.bag.Projection;
 import operators.physcial.Scan;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Optional;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class SimpleIntegrationTest {
-    @Test
-    public void simpleIntegration() throws Exception {
+    Database database;
+    Table boats;
+    Table reserves;
+    Table sailors;
+
+    @Before
+    public void setUp() throws Exception {
         Path inputDir = Paths.get("resources/samples/input/db");
         inputDir = inputDir.toAbsolutePath();
         Optional<Database> databaseOptional = Database.loadDatabase(inputDir);
 
-        Database database = databaseOptional.get();
+        database = databaseOptional.get();
 
-        Optional<Table> boats = database.getTable("Boats");
-        Optional<Table> reserves = database.getTable("Reserves");
+        this.boats = database.getTable("Boats").get();
+        this.reserves = database.getTable("Reserves").get();
+        this.sailors = database.getTable("Sailors").get();
+    }
 
-        Optional<Scan> boatsScan = Scan.setupScan(boats.get());
-        Optional<Scan> reservesScan = Scan.setupScan(reserves.get());
+    @Test
+    public void simpleIntegration() throws Exception {
+        Optional<Scan> boatsScan = Scan.setupScan(boats);
+        Optional<Scan> reservesScan = Scan.setupScan(reserves);
 
         Join join = new Join(boatsScan.get(), reservesScan.get());
 
-        boolean printedHeader = false;
         Optional<Tuple> row;
+
+        System.out.println(join.getHeader().toHeaderForm());
 
         int i = 0;
         while ((row = join.getNextTuple()).isPresent()) {
-            if (!printedHeader) {
-                System.out.println(row.get().header.toHeaderForm());
-                printedHeader = true;
-            }
 
-            System.out.println(i++ + ": " + row.get().toRowForm());
+            System.out.println(++i + ": " + row.get().toRowForm());
         }
+
+        assertThat(i, is(5 * 6));
+    }
+
+    @Test
+    public void projectionTest() throws Exception {
+        Optional<Scan> boatsScan = Scan.setupScan(boats);
+        Optional<Scan> reservesScan = Scan.setupScan(reserves);
+
+        TableHeader tableHeader = new TableHeader(
+                new ArrayList<>(),
+                new ArrayList<>()
+        );
+
+        tableHeader.columnAliases.add("Boats");
+        tableHeader.columnHeaders.add("D");
+        tableHeader.columnAliases.add("");
+        tableHeader.columnHeaders.add("E");
+        tableHeader.columnAliases.add("Reserves");
+        tableHeader.columnHeaders.add("G");
+
+        Join join = new Join(boatsScan.get(), reservesScan.get());
+        Projection projection = new Projection(tableHeader, join);
+
+        Optional<Tuple> row;
+
+        System.out.println(projection.getHeader().toHeaderForm());
+
+        int i = 0;
+        while ((row = projection.getNextTuple()).isPresent()) {
+            System.out.println(++i + ": " + row.get().toRowForm());
+        }
+
+        assertThat(i, is(5 * 6));
     }
 }
