1235f0c5fe6c9f1ad79d08e4ed66643066cc8627
Antoine Salon <ajs672@cornell.edu>
2017-09-20 11:49:26 -0400

    Began work on expression visitor for WHERE clause
    Marked nodes not needed for the assignement and initial implementation

--- project-2/src/query/FilterBuilderVisitor.java
+++ project-2/src/query/FilterBuilderVisitor.java
@@ -1,5 +1,6 @@
 package query;
 
+import datastore.Tuple;
 import net.sf.jsqlparser.expression.*;
 import net.sf.jsqlparser.expression.operators.arithmetic.*;
 import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
@@ -7,66 +8,55 @@ import net.sf.jsqlparser.expression.operators.conditional.OrExpression;
 import net.sf.jsqlparser.expression.operators.relational.*;
 import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.select.SubSelect;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class FilterBuilderVisitor implements ExpressionVisitor {
-    @Override
-    public void visit(NullValue nullValue) {
 
+    private boolean result;
+    private long value;
+
+    private Expression expressionRoot;
+    private Tuple tuple;
+
+    public FilterBuilderVisitor(Expression expression) {
+        this.result = false;
+        this.value = 0;
+        this.expressionRoot = expression;
+    }
+
+    public boolean matches(Tuple tuple) {
+        this.result = false;
+        this.value = 0;
+        this.tuple = tuple;
+
+        expressionRoot.accept(this);
+
+        return this.result;
     }
 
     @Override
-    public void visit(Function function) {
-
-    }
-
-    @Override
-    public void visit(InverseExpression inverseExpression) {
-
-    }
-
-    @Override
-    public void visit(JdbcParameter jdbcParameter) {
-
-    }
-
-    @Override
-    public void visit(DoubleValue doubleValue) {
-
+    public void visit(Column column) {
+        // TODO: implement alias/column name resolution and lookup from tuple
     }
 
     @Override
     public void visit(LongValue longValue) {
-
-    }
-
-    @Override
-    public void visit(DateValue dateValue) {
-
-    }
-
-    @Override
-    public void visit(TimeValue timeValue) {
-
-    }
-
-    @Override
-    public void visit(TimestampValue timestampValue) {
-
+        value = longValue.getValue();
     }
 
     @Override
     public void visit(Parenthesis parenthesis) {
-
-    }
-
-    @Override
-    public void visit(StringValue stringValue) {
-
+        parenthesis.getExpression().accept(this);
     }
 
     @Override
     public void visit(Addition addition) {
+        addition.getLeftExpression().accept(this);
+        long lhs = this.value;
+        addition.getRightExpression().accept(this);
+        long rhs = this.value;
 
+        this.value = lhs + rhs;
     }
 
     @Override
@@ -86,7 +76,12 @@ public class FilterBuilderVisitor implements ExpressionVisitor {
 
     @Override
     public void visit(AndExpression andExpression) {
+        andExpression.getLeftExpression().accept(this);
+        boolean lhs = this.result;
+        andExpression.getRightExpression().accept(this);
+        boolean rhs = this.result;
 
+        this.result = lhs && rhs;
     }
 
     @Override
@@ -95,13 +90,13 @@ public class FilterBuilderVisitor implements ExpressionVisitor {
     }
 
     @Override
-    public void visit(Between between) {
-
-    }
-
-    @Override
     public void visit(EqualsTo equalsTo) {
+        equalsTo.getLeftExpression().accept(this);
+        long lhs = this.value;
+        equalsTo.getRightExpression().accept(this);
+        long rhs = this.value;
 
+        this.result = (lhs == rhs);
     }
 
     @Override
@@ -111,21 +106,6 @@ public class FilterBuilderVisitor implements ExpressionVisitor {
 
     @Override
     public void visit(GreaterThanEquals greaterThanEquals) {
-
-    }
-
-    @Override
-    public void visit(InExpression inExpression) {
-
-    }
-
-    @Override
-    public void visit(IsNullExpression isNullExpression) {
-
-    }
-
-    @Override
-    public void visit(LikeExpression likeExpression) {
 
     }
 
@@ -145,62 +125,122 @@ public class FilterBuilderVisitor implements ExpressionVisitor {
     }
 
     @Override
-    public void visit(Column column) {
+    public void visit(Between between) {
+        throw new NotImplementedException();
+    }
 
+    @Override
+    public void visit(InExpression inExpression) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(IsNullExpression isNullExpression) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(LikeExpression likeExpression) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(NullValue nullValue) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(Function function) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(InverseExpression inverseExpression) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(JdbcParameter jdbcParameter) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(DoubleValue doubleValue) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(StringValue stringValue) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(DateValue dateValue) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(TimeValue timeValue) {
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public void visit(TimestampValue timestampValue) {
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(SubSelect subSelect) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(CaseExpression caseExpression) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(WhenClause whenClause) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(ExistsExpression existsExpression) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(AllComparisonExpression allComparisonExpression) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(AnyComparisonExpression anyComparisonExpression) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(Concat concat) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(Matches matches) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(BitwiseAnd bitwiseAnd) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(BitwiseOr bitwiseOr) {
-
+        throw new NotImplementedException();
     }
 
     @Override
     public void visit(BitwiseXor bitwiseXor) {
-
+        throw new NotImplementedException();
     }
 }
