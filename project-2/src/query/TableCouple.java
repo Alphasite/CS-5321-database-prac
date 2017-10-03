9383eea4c29c3ad5c28a39b586a34214d79d6be1
Nishad <nishadmathur@gmail.com>
2017-10-03 13:42:39 -0400

    Document the table couple class.

--- project-2/src/query/TableCouple.java
+++ project-2/src/query/TableCouple.java
@@ -3,10 +3,20 @@ package query;
 import db.Utilities;
 import net.sf.jsqlparser.schema.Table;
 
+/**
+ * A pair class to encapsulate a pair of tables.
+ */
 public class TableCouple {
     private Table table1;
     private Table table2;
 
+    /**
+     * This pair class will sort Table1 and table 2 class, by lexical order,
+     * to ensure that the tables are always in the same order, regardless of the order in the code.
+     *
+     * @param table1 The first table
+     * @param table2 The second table
+     */
     public TableCouple(Table table1, Table table2){
         String id1 = Utilities.getIdentifier(table1);
         String id2 = Utilities.getIdentifier(table2);
@@ -30,6 +40,12 @@ public class TableCouple {
         return table2;
     }
 
+    /**
+     * Compare based on the names of the tables.
+     *
+     * @param o the object which is being compared too.
+     * @return whether or not they are equal.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -43,10 +59,14 @@ public class TableCouple {
         String table2Id1 = Utilities.getIdentifier(that.table1);
         String table2Id2 = Utilities.getIdentifier(that.table2);
 
-        if (!table1Id1.equals(table2Id1)) return false;
-        return table1Id2.equals(table2Id2);
+        return table1Id1.equals(table2Id1) && table1Id2.equals(table2Id2);
     }
 
+    /**
+     * A hash based on the names of the table.
+     *
+     * @return The has value.
+     */
     @Override
     public int hashCode() {
         String id1 = Utilities.getIdentifier(table1);
