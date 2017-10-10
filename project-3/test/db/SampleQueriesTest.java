a6e4c6016c056394a1cb6e7bfe105ccbeead6286
Tyler Ishikawa <tyi3@cornell.edu>
2017-10-10 04:03:37 -0400

    Get tuples using QueryBuilder rather than from file

--- project-3/test/db/SampleQueriesTest.java
+++ project-3/test/db/SampleQueriesTest.java
@@ -1,59 +1,95 @@
 package db;
 
+import db.datastore.Database;
 import db.datastore.tuple.Tuple;
 import db.datastore.tuple.binary.BinaryTupleReader;
+import db.operators.physical.Operator;
+import db.query.QueryBuilder;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collection;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
+@RunWith(Parameterized.class)
 public class SampleQueriesTest {
 
+    private static String INPUT_PATH = "resources/samples/input";
     private static String OUTPUT_PATH = "resources/samples/output";
     private static String EXPECTED_PATH = "resources/samples/expected";
 
-    @Test
-    public void testSampleQueries() throws Exception {
-        Project3.main(new String[0]);
+    @Parameters(name = "{1}")
+    public static Collection<Object[]> data() {
+        ArrayList<Object[]> testCases = new ArrayList<>();
 
-        for (int i = 1; i <= 15; i++) {
-            System.out.println("Checking file 'query" + i + "'...");
+        Database DB = Database.loadDatabase(Paths.get(INPUT_PATH + File.separator + "db"));
+        QueryBuilder builder = new QueryBuilder(DB);
 
-            File outputFile = new File(OUTPUT_PATH + File.separator + "query" + i);
+        try {
+            CCJSqlParser parser = new CCJSqlParser(new FileReader(INPUT_PATH + File.separator + "queries.sql"));
+            Statement statement;
+            int i = 1;
 
-            assertTrue("file 'query" + i + "' is not in output directory", outputFile.exists());
+            while ((statement = parser.Statement()) != null) {
+                PlainSelect select = (PlainSelect) ((Select) statement).getSelectBody();
+                Operator queryPlanRoot = builder.buildQuery(select);
 
-            File expectedFile = new File(EXPECTED_PATH + File.separator + "query" + i);
 
-            BinaryTupleReader outputReader = new BinaryTupleReader(null, new FileInputStream(outputFile).getChannel());
-            BinaryTupleReader expectedReader = new BinaryTupleReader(null, new FileInputStream(expectedFile).getChannel());
+                File expectedFile = new File(EXPECTED_PATH + File.separator + "query" + i);
+                testCases.add(new Object[]{queryPlanRoot, expectedFile});
 
-            Tuple outputTuple = null;
-            Tuple expectedTuple;
-
-            while (true) {
-                expectedTuple = expectedReader.next();
-
-                try {
-                    outputTuple = outputReader.next();
-                } catch (OutOfMemoryError e) {
-                    fail("tuple has invalid medidata");
-                }
-
-                if (outputTuple == null && expectedTuple == null) {
-                    break;
-                } else if (outputTuple == null) {
-                    fail("output file has fewer pages than expected");
-                } else if (expectedTuple == null) {
-                    fail("output file has more pages than expected");
-                } else {
-                    assertEquals(expectedTuple, outputTuple);
-                }
+                i++;
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
+        return testCases;
+    }
+
+    private Operator queryPlanRoot;
+    private BinaryTupleReader reader;
+
+    public SampleQueriesTest(Operator queryPlanRoot, File expectedFile) {
+        this.queryPlanRoot = queryPlanRoot;
+
+        try {
+            this.reader = new BinaryTupleReader(null, new FileInputStream(expectedFile).getChannel());
+        } catch (FileNotFoundException e) {
+            fail("file '" + expectedFile.getName() + "' not found in expected folder");
+        }
+    }
+
+    @Test
+    public void test() {
+        while (true) {
+            Tuple expectedTuple = reader.next();
+            Tuple outputTuple = queryPlanRoot.getNextTuple();
+
+            if (outputTuple == null && expectedTuple == null) {
+                break;
+            } else if (outputTuple == null) {
+                fail("output has fewer tuples than expected");
+            } else if (expectedTuple == null) {
+                fail("output has more tuples than expected");
+            } else {
+                assertEquals(expectedTuple, outputTuple);
+            }
+        }
     }
 
 }
