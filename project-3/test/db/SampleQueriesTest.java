e74477c554f5df4f28ff0e21a9a2b5e1ce079bb0
Tyler Ishikawa <tyi3@cornell.edu>
2017-10-10 00:47:59 -0400

    create test for sample queries

---
+++ project-3/test/db/SampleQueriesTest.java
@@ -0,0 +1,63 @@
+package db;
+
+import db.datastore.tuple.Tuple;
+import db.datastore.tuple.binary.BinaryTupleReader;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import static org.junit.Assert.*;
+
+public class SampleQueriesTest {
+
+    private static String OUTPUT_PATH = "resources/samples/output";
+    private static String EXPECTED_PATH = "resources/samples/expected";
+
+    @Before
+    public void setUp() throws Exception {
+        Project3.DUMP = false;
+    }
+
+    @Test
+    public void testSampleQueries() throws Exception {
+        Project3.main(new String[]{});
+
+        File[] outputFiles = new File(OUTPUT_PATH).listFiles();
+        File[] expectedFiles = new File(EXPECTED_PATH).listFiles();
+
+        for (int i = 1; i <= 15; i++) {
+            System.out.println("Checking file 'query" + i + "'...");
+
+            File outputFile = new File(OUTPUT_PATH + File.separator + "query" + i);
+
+            assertTrue("Error: file 'query" + i + "' is not in output directory", outputFile.exists());
+
+            File expectedFile = new File(EXPECTED_PATH + File.separator + "query" + i);
+
+            BinaryTupleReader outputReader = new BinaryTupleReader(null, new FileInputStream(outputFile).getChannel());
+            BinaryTupleReader expectedReader = new BinaryTupleReader(null, new FileInputStream(expectedFile).getChannel());
+
+            Tuple outputTuple;
+            Tuple expectedTuple;
+
+            while (true) {
+                outputTuple = outputReader.next();
+                expectedTuple = expectedReader.next();
+
+                if (outputTuple == null && expectedTuple == null) {
+                    break;
+                } else if (outputTuple == null) {
+                    fail("Error: output file has fewer pages than expected");
+                } else if (expectedTuple == null) {
+                    fail("Error: output file has more pages than expected");
+                } else {
+                    assertEquals(expectedTuple, outputTuple);
+                }
+            }
+        }
+
+    }
+
+}
