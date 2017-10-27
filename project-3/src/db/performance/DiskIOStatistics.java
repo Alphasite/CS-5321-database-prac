f7423dcc8a5602aacb8a88f024bd35ed2a4f4d37
Nishad <nishadmathur@gmail.com>
2017-10-26 21:50:19 -0400

    Doc the performance package.

--- project-3/src/db/performance/DiskIOStatistics.java
+++ project-3/src/db/performance/DiskIOStatistics.java
@@ -1,5 +1,8 @@
 package db.performance;
 
+/**
+ * A class to store the disk io stats for the system.
+ */
 public class DiskIOStatistics {
     public static Integer reads = 0;
     public static Integer writes = 0;
