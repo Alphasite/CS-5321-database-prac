c95c28f526e78d67cbd47544483cc2880310beca
Tyler Ishikawa <tyi3@cornell.edu>
2017-11-07 21:58:16 -0500

    change indices to single index in tableInfo

--- project-3/src/db/datastore/Database.java
+++ project-3/src/db/datastore/Database.java
@@ -83,7 +83,7 @@ public class Database {
 
                 // add to tableInfo
                 TableInfo thisIndexTable = tables.get(info.tableName);
-                thisIndexTable.indices.put(info.attributeName, info);
+                thisIndexTable.index = info;
             }
         } catch (IOException e) {
             // No index_info.txt found
