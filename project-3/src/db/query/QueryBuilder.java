6cadd7e460cf3ae743fb7663ae67063925287103
Antoine Salon <ajs672@cornell.edu>
2017-11-12 19:44:22 -0500

    Cleaned up QueryBuilder name / alias handling

--- project-3/src/db/query/QueryBuilder.java
+++ project-3/src/db/query/QueryBuilder.java
@@ -24,10 +24,47 @@ public class QueryBuilder {
     private Database db;
 
     /**
+     * List of identifying table names (either original name or alias defined in FROM clause).
+     * Used by other tokens in query (SELECT, WHERE, ORDER BY).
+     */
+    private List<String> tableIdentifiers;
+
+    /**
+     * Map every table <em>identifier</em> to the corresponding logical operator used to access its tuples
+     * (ie. Scan or Scan + Rename).
+     */
+    private Map<String, LogicalOperator> tableOperators;
+
+    /**
      * Initialize query builder using the provided database object as a source for Table schema information
      */
     public QueryBuilder(Database db) {
         this.db = db;
+        this.tableIdentifiers = new ArrayList<>();
+        this.tableOperators = new HashMap<>();
+    }
+
+    /**
+     * Populate internal structures with name/alias info for all referenced tables and create Scan/Rename operators
+     *
+     * @param fromTable The leftmost table in the join
+     * @param joins     All other joins
+     */
+    private void initTableInfo(Table fromTable, List<Join> joins) {
+        // reset tables
+        tableIdentifiers.clear();
+        tableOperators.clear();
+
+        tableIdentifiers.add(Utilities.getIdentifier(fromTable));
+        tableOperators.put(Utilities.getIdentifier(fromTable), getScanAndMaybeRename(fromTable));
+
+        if (joins != null) {
+            for (Join join : joins) {
+                Table joinTable = (Table) join.getRightItem();
+                tableIdentifiers.add(Utilities.getIdentifier(joinTable));
+                tableOperators.put(Utilities.getIdentifier(joinTable), getScanAndMaybeRename(joinTable));
+            }
+        }
     }
 
     /**
@@ -42,21 +79,17 @@ public class QueryBuilder {
     public LogicalOperator buildQuery(PlainSelect query) {
         // Store ref to all needed query tokens
         List<SelectItem> selectItems = query.getSelectItems();
-        Table fromItem = (Table) query.getFromItem();
-        List<Join> joinItems = query.getJoins();
         Expression whereItem = query.getWhere();
         List<OrderByElement> orderBy = query.getOrderByElements();
         boolean isDistinct = query.getDistinct() != null;
 
-        if (joinItems == null) {
-            joinItems = new ArrayList<>();
-        }
+        this.initTableInfo((Table) query.getFromItem(), query.getJoins());
 
         // Keep reference to current root
         LogicalOperator rootNode;
 
         // Build the scan-select-join tree structure
-        rootNode = processWhereClause(whereItem, fromItem, joinItems);
+        rootNode = processWhereClause(whereItem);
 
         // Add projections
         if (!(selectItems.get(0) instanceof AllColumns)) {
@@ -79,16 +112,7 @@ public class QueryBuilder {
 
             TableHeader header = rootNode.getHeader();
 
-            List<String> tableNames = new ArrayList<>();
-
-            tableNames.add(Utilities.getIdentifier(fromItem));
-
-            for (Join joinItem : joinItems) {
-                String alias = Utilities.getIdentifier((Table) joinItem.getRightItem());
-                tableNames.add(alias);
-            }
-
-            for (String alias : tableNames) {
+            for (String alias : this.tableIdentifiers) {
                 for (int i = 0; i < header.size(); i++) {
                     if (header.columnAliases.get(i).equals(alias)) {
                         aliases.add(alias);
@@ -103,20 +127,20 @@ public class QueryBuilder {
         // The spec allows handling sorting and duplicate elimination after projection
 
         if (orderBy != null) {
-            List<String> aliases = new ArrayList<>();
+            List<String> referencedTables = new ArrayList<>();
             List<String> columns = new ArrayList<>();
 
             for (OrderByElement element : orderBy) {
                 Column columnInstance = (Column) element.getExpression();
 
-                String alias = Utilities.getIdentifier(columnInstance.getTable());
+                String tableId = Utilities.getIdentifier(columnInstance.getTable());
                 String column = columnInstance.getColumnName();
 
-                aliases.add(alias);
+                referencedTables.add(tableId);
                 columns.add(column);
             }
 
-            TableHeader sortHeader = new TableHeader(aliases, columns);
+            TableHeader sortHeader = new TableHeader(referencedTables, columns);
             rootNode = new LogicalSortOperator(rootNode, sortHeader);
         }
 
@@ -137,32 +161,14 @@ public class QueryBuilder {
      * Build the internal maps used to link every part of the WHERE clause to the table they reference
      *
      * @param rootExpression the where expression
-     * @param joinRootTable the left most table on the expression tree.
-     * @param rightJoinExpressions the non root joined tables and their aliases
      * @return The root node of the sql tree formed from the where and from conditions
      */
-    private LogicalOperator processWhereClause(Expression rootExpression, Table joinRootTable, List<Join> rightJoinExpressions) {
+    private LogicalOperator processWhereClause(Expression rootExpression) {
         // No root node.
         LogicalOperator rootNode = null;
 
-        // Then find which other tables exist.
-        // then store them in a list of tables which haven't yet been joined.
-
-        Map<String, LogicalOperator> joinableTableInstances = new HashMap<>();
-
+        Set<String> tablesToJoin = new HashSet<>(this.tableIdentifiers);
         Set<String> joinedTables = new HashSet<>();
-        Set<String> unjoinedTables = new HashSet<>();
-
-        // Since the root table is a special case, handle that.
-        unjoinedTables.add(Utilities.getIdentifier(joinRootTable));
-        joinableTableInstances.put(Utilities.getIdentifier(joinRootTable), this.getScanAndMaybeRename(joinRootTable));
-
-        for (Join join : rightJoinExpressions) {
-            Table table = (Table) join.getRightItem();
-            String identifier = Utilities.getIdentifier(table);
-            joinableTableInstances.put(identifier, this.getScanAndMaybeRename(table));
-            unjoinedTables.add(identifier);
-        }
 
         // Decompose the expression tree and then add the root nodes expressions to the root node.
         Map<String, Expression> selectionExpressions = new HashMap<>();
@@ -177,14 +183,14 @@ public class QueryBuilder {
         }
 
         TriFunction<LogicalOperator, String, Expression, LogicalOperator> joinTable = (root, tableName, expression) -> {
-            LogicalOperator rightOp = joinableTableInstances.get(tableName);
+            LogicalOperator rightOp = this.tableOperators.get(tableName);
 
             if (selectionExpressions.containsKey(tableName)) {
                 rightOp = new LogicalSelectOperator(rightOp, selectionExpressions.get(tableName));
             }
 
             joinedTables.add(tableName);
-            unjoinedTables.remove(tableName);
+            tablesToJoin.remove(tableName);
 
             if (root != null) {
                 return new LogicalJoinOperator(root, rightOp, expression);
@@ -214,7 +220,7 @@ public class QueryBuilder {
             }
         }
 
-        for (String table : unjoinedTables) {
+        for (String table : tablesToJoin) {
             rootNode = joinTable.apply(rootNode, table, null);
         }
 
